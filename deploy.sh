a2a663077a56a5ac7956201ee83b365923aa3b9e
Matthew Carroll <matthewCmatt@gmail.com>
2023-09-02 05:42:01 -0500

    Changed deploy script to not attempt password. User must enter manually.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # A script to deploy the site to OU's servers by SFTP
+# Note: You can upload individual files on your own to avoid re-uploading the whole site.
 sftp ouok0000@ouwww.ou.edu <<SFTP_ROUTINE
-DriveVehicles7
+rm *
 put *.html
 put *.css
 put -r people/
