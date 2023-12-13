0036d68017d3bdfa46ce4c2839322d8adaa74982
Matthew Carroll <matthewCmatt@gmail.com>
2023-12-13 17:58:25 -0600

    Tweaked mobile-NAV

--- src/assets/js/mobile-nav.js
+++ src/assets/js/mobile-nav.js
@@ -1,19 +1,20 @@
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menuItem");
-const hamburger = document.querySelector(".hamburger");
 const closeIcon = document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
 
 function toggleMenu() {
     if (menu.classList.contains("showMenu")) {
+        // Hide the menu
         menu.classList.remove("showMenu");
+        menu.classList.add("hideMenu");
         closeIcon.style.display = "none";
         menuIcon.style.display = "block";
     } else {
+        // Show the menu
+        menu.classList.remove("hideMenu");
         menu.classList.add("showMenu");
         closeIcon.style.display = "block";
         menuIcon.style.display = "none";
     }
 }
-
-hamburger.addEventListener("click", toggleMenu);
