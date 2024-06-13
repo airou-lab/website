a86cf39f36bc40eae5ec93bef4fd340121d4b5b6
AkselCan <aksel2606@gmail.com>
2024-06-13 11:38:15 -0500

    changed sftp routine

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # Note: You can upload individual files on your own to avoid re-uploading the whole site.
 npm run clean
 npm run build
-sftp ouok0000@ouwww.ou.edu <<SFTP_ROUTINE
+sftp airou@pearl.cs.nor.ou.edu <<SFTP_ROUTINE
 put -r _site/*
 bye
 SFTP_ROUTINE
